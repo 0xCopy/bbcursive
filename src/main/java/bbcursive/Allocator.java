c109bbb1cfba82a55a16423b77205c506885c82e
jnorthrup <northrup.james@gmail.com>
2015-05-11 16:04:31 -0700

    bbcursive,1xio,rxf touched in this commit -- need to bump versions buyer beware
    provides and uses tighter parseInt and allocators to close the gap between rxf and the rest.
    
     overall profiler shows roughly equivalent benchmark camel humps in the cpu score.
    
     ![image](https://cloud.githubusercontent.com/assets/73514/7576808/b132a70e-f7f6-11e4-9780-4847330e0426.png)
    
     benchmarks seem to show allocation of Direct ByteBuffers allocated in force in rxf, with the other 3 implementations presumably using heap or possibly fast "Unsafe" shortcuts.
    
     ![image](https://cloud.githubusercontent.com/assets/73514/7576817/c77eab5c-f7f6-11e4-974a-e1e79d304705.png)

---
+++ src/main/java/bbcursive/Allocator.java
@@ -0,0 +1,90 @@
+package bbcursive;
+
+import java.nio.ByteBuffer;
+
+import static bbcursive.std.log;
+
+/**
+ * User: jim
+ * Date: Oct 6, 2007
+ * Time: 3:10:32 AM
+ */
+public class Allocator {
+
+     ByteBuffer DIRECT_HEAP;
+    static public int MEG = (1<<10)<<10,BLOCKSIZE=MEG*2;
+
+    private  int initialCapacity =Runtime.getRuntime().availableProcessors()*20*2;
+    final private  boolean test = true;
+    public  final ByteBuffer EMPTY_SET = ByteBuffer.allocate(0).asReadOnlyBuffer() ;
+
+     int size = initialCapacity;
+
+    public Allocator(int... megs) {
+        if(megs.length>0)
+        this.initialCapacity = megs[0];
+
+        ByteBuffer buffer = null;
+        while (buffer == null)
+            try {
+
+                if (isDirect())
+                    buffer = (ByteBuffer) ByteBuffer.allocateDirect(size) .limit(0);
+                else
+                    buffer = (ByteBuffer) ByteBuffer.allocate(size) .limit(0);
+
+                DIRECT_HEAP = buffer;
+                log("Heap allocated at " + size / MEG + " megs");
+                size *= 2;
+
+            } catch (IllegalArgumentException e) {
+                size = Math.max(16 * MEG, size / 2);
+                System.gc();
+            } catch (OutOfMemoryError e) {
+                size = Math.max(16 * MEG, size / 2);
+                System.gc();
+            }
+    }
+
+    private  void init() {
+
+        ByteBuffer buffer = null;
+        while (buffer == null)
+            try {
+
+                if (isDirect())
+                    buffer = (ByteBuffer) ByteBuffer.allocateDirect(size) .limit(0);
+                else
+                    buffer = (ByteBuffer) ByteBuffer.allocate(size) .limit(0);
+
+                DIRECT_HEAP = buffer;
+                log("Heap allocated at " + size / MEG + " megs");
+                size *= 2;
+
+            } catch (IllegalArgumentException e) {
+                size = Math.max(16 * MEG, size / 2);
+                System.gc();
+            } catch (OutOfMemoryError e) {
+                size = Math.max(16 * MEG, size / 2);
+                System.gc();
+            }
+    }
+
+    ByteBuffer allocate(int size) {
+        if (size == 0) return EMPTY_SET;
+        try {
+            DIRECT_HEAP.limit(DIRECT_HEAP.limit() + size);
+        } catch (IllegalArgumentException e) {
+            init();
+            return allocate(size);
+        }
+        ByteBuffer ret = (ByteBuffer) DIRECT_HEAP.slice().limit(size).mark();
+        DIRECT_HEAP.position(DIRECT_HEAP.limit());
+        return ret;
+    }
+
+    public  boolean isDirect() {
+        return false;
+    }
+
+}
--- src/main/java/bbcursive/std.java
+++ src/main/java/bbcursive/std.java
@@ -11,194 +11,322 @@ import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static bbcursive.Cursive.pre.*;
+import static bbcursive.Cursive.pre.debug;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
-* Created by jim on 8/8/14.
-*/
+ * Created by jim on 8/8/14.
+ */
 public class std {
-  public static <T extends ByteBuffer> T bb(T b, Cursive... ops) {
-    for (int i = 0, opsLength = ops.length; i < opsLength; i++) {
-      Cursive op = ops[i];
-      b = op.f(b);
+    public static <T extends ByteBuffer> T bb(T b, Cursive... ops) {
+        for (int i = 0, opsLength = ops.length; i < opsLength; i++) {
+            Cursive op = ops[i];
+            b = op.f(b);
+        }
+        return b;
     }
-    return b;
-  }
-  public static <T extends ByteBuffer,S extends WantsZeroCopy> T bb(S b, Cursive... ops) {
-    ByteBuffer b1 = b.asByteBuffer();
-    for (int i = 0, opsLength = ops.length; i < opsLength; i++) {
-      Cursive op = ops[i];
-      b1 = op.f(b1);
+
+    public static <T extends ByteBuffer, S extends WantsZeroCopy> T bb(S b, Cursive... ops) {
+        ByteBuffer b1 = b.asByteBuffer();
+        for (int i = 0, opsLength = ops.length; i < opsLength; i++) {
+            Cursive op = ops[i];
+            b1 = op.f(b1);
+        }
+        return (T) b1;
     }
-    return (T) b1;
-  }
 
-  public static <T extends ByteBufferReader,S extends WantsZeroCopy> T fast(S zc) {
-    return fast(zc.asByteBuffer());
-  }
-
-  public static <T extends ByteBufferReader> T fast(ByteBuffer buf) {
-    T r;
-    try {
-      r=(T) (buf.hasArray() ? new UnsafeHeapByteBufferReader(buf) : new UnsafeDirectByteBufferReader(buf));
-    } catch (UnsupportedOperationException e) {
-      r=(T) new JavaByteBufferReader(buf);
+    public static <T extends ByteBufferReader, S extends WantsZeroCopy> T fast(S zc) {
+        return fast(zc.asByteBuffer());
     }
-    return r;
-  }
 
-  /**
-   * convenience method
-   *
-   * @param bytes
-   * @param operations
-   * @return
-   */
-  public static String str(ByteBuffer bytes, Cursive... operations) {
-    for (Cursive operation : operations) {
-      if (operation instanceof pre) {
-        bytes = operation.f(bytes);
-      }
+    public static <T extends ByteBufferReader> T fast(ByteBuffer buf) {
+        T r;
+        try {
+            r = (T) (buf.hasArray() ? new UnsafeHeapByteBufferReader(buf) : new UnsafeDirectByteBufferReader(buf));
+        } catch (UnsupportedOperationException e) {
+            r = (T) new JavaByteBufferReader(buf);
+        }
+        return r;
     }
-    String s = UTF_8.decode(bytes).toString();
-    for (Cursive operation : operations) {
-      if (operation instanceof post) {
-        bytes = operation.f(bytes);
-      }
+
+    /**
+     * convenience method
+     *
+     * @param bytes
+     * @param operations
+     * @return
+     */
+    public static String str(ByteBuffer bytes, Cursive... operations) {
+        for (Cursive operation : operations) {
+            if (operation instanceof pre) {
+                bytes = operation.f(bytes);
+            }
+        }
+        String s = UTF_8.decode(bytes).toString();
+        for (Cursive operation : operations) {
+            if (operation instanceof post) {
+                bytes = operation.f(bytes);
+            }
+        }
+        return s;
     }
-    return s;
-  }
 
-  /**
-   * just saves a few chars
-   * @param something toString will run on this
-   * @param atoms
-   * @return
-   */
-  public static String str(WantsZeroCopy something, Cursive... atoms) {
-    return str(something.asByteBuffer(),atoms);
-  }
-/**
-   * just saves a few chars
-   * @param something toString will run on this
-   * @param atoms
-   * @return
-   */
-  public static String str(AtomicReference<?extends WantsZeroCopy> something, Cursive... atoms) {
-    return str(something.get(),atoms);
-  }
-
-  /**
-   * just saves a few chars
-   * @param something toString will run on this
-   * @return
-   */
-  public static String str(Object something) {
-    return String.valueOf(something);
-  }
-
-  /**
-   * convenience method
-   *
-   * @param src
-   * @param operations
-   * @return
-   */
-  public static <T extends CharSequence> ByteBuffer bb(T src, Cursive... operations) {
-
-    ByteBuffer byteBuffer = UTF_8.encode(src.toString());
-    for (Cursive operation : operations) {
-      byteBuffer = operation.f(byteBuffer);
+    /**
+     * just saves a few chars
+     *
+     * @param something toString will run on this
+     * @param atoms
+     * @return
+     */
+    public static String str(WantsZeroCopy something, Cursive... atoms) {
+        return str(something.asByteBuffer(), atoms);
     }
-    return byteBuffer;
-  }
 
-  /**
-   *
-   * @param src
-   * @param dest
-   * @param <R>
-   * @param <S>
-   * @return
-   */
-
-  public static <R extends ByteBuffer, S extends ByteBuffer> R push(S src, R dest) {
-    int need = src
-        .remaining(),
-        have = dest.remaining();
-    if (have > need) {
-      return (R) dest.put(src);
+    /**
+     * just saves a few chars
+     *
+     * @param something toString will run on this
+     * @param atoms
+     * @return
+     */
+    public static String str(AtomicReference<? extends WantsZeroCopy> something, Cursive... atoms) {
+        return str(something.get(), atoms);
     }
-    dest.put((S) src.slice().limit(have));
-    src.position(src.position() + have);
-    return dest;
-  }
 
-  public static <T extends ByteBuffer, S extends ByteBuffer> T grow(S src) {
-    return (T) ByteBuffer.allocateDirect(src.capacity() << 1).put(src);
-  }
+    /**
+     * just saves a few chars
+     *
+     * @param something toString will run on this
+     * @return
+     */
+    public static String str(Object something) {
+        return String.valueOf(something);
+    }
 
-  /**
-   * conditional debug output assert log(Object,[prefix[,suffix]])
-   *
-   * @param ob
-   * @param prefixSuffix
-   * @return
-   */
-  public static void log(Object ob, String... prefixSuffix) {
-    assert log$(ob, prefixSuffix);
-  }
+    public static Integer parseInt(ByteBuffer r) {
+        long x = 0;
+        boolean neg = false;
 
-  ;
+        Integer res = null;
+        if (r.hasRemaining()) {
+            int i = r.get();
+            switch (i) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    x = x * 10 + i - '0';
+                    break;
+                case '-':
+                    neg = true;
+                case '+':
 
-  /**
-   * conditional debug output assert log(Object,[prefix[,suffix]])
-   *
-   * @param ob
-   * @param prefixSuffix
-   * @return
-   */
-  public static boolean log$(Object ob, String... prefixSuffix) {
-    boolean hasSuffix = prefixSuffix.length > 1;
-    if (prefixSuffix.length > 0)
-      System.err.print(prefixSuffix[0] + "\t");
-    if (ob instanceof ByteBuffer) {
-      bb((ByteBuffer) ob, debug);
-    } else if (ob instanceof WantsZeroCopy) {
-      WantsZeroCopy wantsZeroCopy = (WantsZeroCopy) ob;
-      bb(wantsZeroCopy.asByteBuffer(),debug);
-    }else
+            }
+            while (r.hasRemaining()) {
+                i = r.get();
+                switch (i) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        x = x * 10 + i - '0';
+                        break;
+                    case '-':
+                        neg = true;
+                    case '+':
+                            break;
+
+                }
+            }
+
+
+            res = (int) ((neg ? -x : x) & 0xffffffffL);
+        }
+        return res;
+    }
+    /**
+     * convenience method
+     *
+     * @param src
+     * @param operations
+     * @return
+     */
+    public static <T extends CharSequence> ByteBuffer bb(T src, Cursive... operations) {
+
+        ByteBuffer byteBuffer = UTF_8.encode(src.toString());
+        for (Cursive operation : operations) {
+            byteBuffer = operation.f(byteBuffer);
+        }
+        return byteBuffer;
+    }
+
+    public static Integer parseInt(String r)
     {
-      bb(String.valueOf(ob), debug);
-    }
-    if (hasSuffix)
-      System.err.println(prefixSuffix[1] + "\t");
-    return true;
-  }
+        long x = 0;
+        boolean neg = false;
 
-  public static ByteBuffer cat(List<ByteBuffer> byteBuffers) {
-    ByteBuffer[] byteBuffers1 = byteBuffers.toArray(new ByteBuffer[byteBuffers.size()]);
-    return cat(byteBuffers1);
-  }
+        Integer res = null;
 
-  public static ByteBuffer cat(ByteBuffer... src) {
-    ByteBuffer cursor;
-    int total = 0;
-    if (1 < src.length) {
-      for (int i = 0, payloadLength = src.length; i < payloadLength; i++) {
-        ByteBuffer byteBuffer = src[i];
-        total += byteBuffer.remaining();
-      }
-      cursor = ByteBuffer.allocateDirect(total);
-      for (int i = 0, payloadLength = src.length; i < payloadLength; i++) {
-        ByteBuffer byteBuffer = src[i];
-        cursor.put(byteBuffer);
-      }
-      cursor.rewind();
-    } else {
-      cursor = src[0];
+
+        int length = r.length();
+        if (length >0) {
+            int i = r.charAt(0);
+            switch (i) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    x = x * 10 + i - '0';
+                    break;
+                case '-':
+                    neg = true;
+                case '+':   break;
+
+            }
+
+            for (int j = 1; j < length; j++) {
+                i = r.charAt(i);
+                switch (i) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        x = x * 10 + i - '0';
+                        break;
+                    case '-':
+                        neg = true;
+                    case '+':
+                                   break;
+                }
+            }
+
+
+            res = (int) ((neg ? -x : x) & 0xffffffffL);
+        }
+        return res;
     }
-    return cursor;
-  }
+
+    /**
+     * @param src
+     * @param dest
+     * @param <R>
+     * @param <S>
+     * @return
+     */
+
+    public static <R extends ByteBuffer, S extends ByteBuffer> R push(S src, R dest) {
+        int need = src
+                .remaining(),
+                have = dest.remaining();
+        if (have > need) {
+            return (R) dest.put(src);
+        }
+        dest.put((S) src.slice().limit(have));
+        src.position(src.position() + have);
+        return dest;
+    }
+
+    public static <T extends ByteBuffer, S extends ByteBuffer> T grow(S src) {
+        return (T) ByteBuffer.allocateDirect(src.capacity() << 1).put(src);
+    }
+
+    /**
+     * conditional debug output assert log(Object,[prefix[,suffix]])
+     *
+     * @param ob
+     * @param prefixSuffix
+     * @return
+     */
+    public static void log(Object ob, String... prefixSuffix) {
+        assert log$(ob, prefixSuffix);
+    }
+
+    ;
+
+    /**
+     * conditional debug output assert log(Object,[prefix[,suffix]])
+     *
+     * @param ob
+     * @param prefixSuffix
+     * @return
+     */
+    public static boolean log$(Object ob, String... prefixSuffix) {
+        boolean hasSuffix = prefixSuffix.length > 1;
+        if (prefixSuffix.length > 0)
+            System.err.print(prefixSuffix[0] + "\t");
+        if (ob instanceof ByteBuffer) {
+            bb((ByteBuffer) ob, debug);
+        } else if (ob instanceof WantsZeroCopy) {
+            WantsZeroCopy wantsZeroCopy = (WantsZeroCopy) ob;
+            bb(wantsZeroCopy.asByteBuffer(), debug);
+        } else {
+            bb(String.valueOf(ob), debug);
+        }
+        if (hasSuffix)
+            System.err.println(prefixSuffix[1] + "\t");
+        return true;
+    }
+
+    public static ByteBuffer cat(List<ByteBuffer> byteBuffers) {
+        ByteBuffer[] byteBuffers1 = byteBuffers.toArray(new ByteBuffer[byteBuffers.size()]);
+        return cat(byteBuffers1);
+    }
+
+    public static Allocator allocator ;
+
+
+    public static ByteBuffer cat(ByteBuffer... src) {
+        ByteBuffer cursor;
+        int total = 0;
+        if (1 >= src.length) {
+            cursor = src[0];
+        } else {
+            for (int i = 0, payloadLength = src.length; i < payloadLength; i++) {
+                ByteBuffer byteBuffer = src[i];
+                total += byteBuffer.remaining();
+            }
+            cursor = alloc(total);
+            for (int i = 0, payloadLength = src.length; i < payloadLength; i++) {
+                ByteBuffer byteBuffer = src[i];
+                cursor.put(byteBuffer);
+            }
+            cursor.rewind();
+        }
+        return cursor;
+    }
+
+    public static ByteBuffer alloc(int size) {
+        if (null != allocator) {
+             return allocator.allocate(size);
+        }else return ByteBuffer.allocateDirect(size);
+     }
+
+    public static ByteBufferReader alloca(int size) {
+        return fast(alloc(size));
+    }
+
 }
