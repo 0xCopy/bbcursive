6a5fa8eca296cc07a94ccea76d0c91c860be0834
jim <jim@vsiwest.com>
2016-01-18 16:04:25 -0800

    some doc clarifications to try to scribe concepts for which I have fleeting understandings when left unobserved

--- src/main/java/bbcursive/vtables/_edge.java
+++ src/main/java/bbcursive/vtables/_edge.java
@@ -11,7 +11,7 @@ import java.util.Objects;
  * <p>
  * <p>
  * left refers to "refreence" side, right refers to "pointer" side.
- * <p/>
+ * <p>
  * User: jim
  * Date: Sep 18, 2008
  * Time: 6:05:14 AM
@@ -26,12 +26,13 @@ public abstract class _edge<coreType, addressType> {
 
     protected abstract addressType goTo(addressType addressType);
 
-    /**this is a single method mutator/emitter
+    /**
+     * this is a single method mutator/emitter
      *
      * @param notnullorself new core
      * @return the core being held now
      */
-     private coreType theCore(coreType... notnullorself) {
+    private coreType theCore(coreType... notnullorself) {
         switch (notnullorself.length) {
             case 0:
                 return core;
@@ -44,33 +45,50 @@ public abstract class _edge<coreType, addressType> {
 
     /**
      * left type node with induction of core only.  address will be null until set
+     *
      * @param e
      * @return
      */
-    public   coreType core(_edge<coreType, addressType>... e) {
+    public coreType core(_edge<coreType, addressType>... e) {
         return e.length == 0 || this != e[0] ? bind(theCore(e[0].core()), e[0].location()).core() : core;
     }
 
-    /**an address
+    /**
+     * an address
+     * <p>
+     * for _ptr, Integer is an address of a ByteBuffer state, linear memory here.
+     * <p>
+     * for {@code Map<K,V>}, K is an address to get a V from {@code _edge<V,K>}
+     * <p>
+     * for {@code _edge<_edge<A,B>,_ptr>}
      *
-     * @param notnullorself
-     * @return
+     * @param notnullorself null for self.  non-empty set for induction
+     * @return typically what is returned is what is passed in most recently to any of the Pair.second mutators (this.at, this.goto, this.location).
      */
     protected final addressType at(addressType... notnullorself) {
         addressType addressType1 = notnullorself[0];
-        return 0 != notnullorself.length && !Objects.equals(this, addressType1) ? goTo(addressType1) : r$();
+        if (0 != notnullorself.length)
+            if (!Objects.equals(this, addressType1))
+                return goTo(addressType1);
+        return r$();
     }
 
+    /**
+     * internal factory or getter for pair.second.  for _ptr this is inferred from bytebuffer instance.
+     *
+     * @return
+     */
     protected abstract addressType r$();
 
     /**
-     *  right type node with induction
+     * right type node with induction
+     *
      * @param e
      * @return
      */
     public final addressType location(_edge<coreType, addressType>... e) {
         _edge<coreType, addressType> coreTypeaddressType_edge;
-        return 0 == e.length || (coreTypeaddressType_edge = _edge.this) != e[0] ? bind((e[0].core()), at(e[0].location())).location() : coreTypeaddressType_edge.location();
+        return 0 == e.length || (coreTypeaddressType_edge = this) != e[0] ? bind((e[0].core()), at(e[0].location())).location() : coreTypeaddressType_edge.location();
     }
 
     /**
@@ -84,7 +102,7 @@ public abstract class _edge<coreType, addressType> {
             , addressType address) {
         theCore(coreType);
         at(address);
-        return _edge.this;
+        return this;
     }
 
 
