870c22012dfba44ddb06635d2955333ccbb4654a
jim <jim@vsiwest.com>
2016-01-30 04:10:07 -0800

    interesting anyOf parralel spurs stackoverflow

--- src/main/java/bbcursive/lib/anyOf_.java
+++ src/main/java/bbcursive/lib/anyOf_.java
@@ -44,83 +44,13 @@ public class anyOf_ {
                 final int[] offsets = {mark, mark};
                 final Set[] flaggs = {NONE_OF};
 
-                Optional<_edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>> edgePtrEdge = Arrays.stream(anyOf).parallel().map(new Function<UnaryOperator<ByteBuffer>, _edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>>() {
+                Optional<_edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>> edgePtrEdge = Arrays.stream(anyOf)/*.parallel()*/.map(new Function<UnaryOperator<ByteBuffer>, _edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>>() {
 
                     @Override
                     public _edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr> apply(UnaryOperator<ByteBuffer> op) {
 
 
-                        return new _edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>() {
-                            @Override
-                            protected _ptr at() {
-                                return r$();
-                            }
-
-                            @Override
-                            protected _ptr goTo(_ptr ptr) {
-                                throw new Error("trifling with an immutable pointer");
-                            }
-
-                            /**
-                             * this binds a pointer to a pair of ByteBuffer and Integer.  note the bytebuffer is mutated by this
-                             * operation and will corrupt the source stream if this isn't a slice or a duplicate
-                             *
-                             *
-                             * @return the _ptr
-                             */
-                            @Override
-
-                            protected _ptr r$() {
-
-                                return (_ptr) new _ptr().bind(
-                                        (ByteBuffer) buffer.duplicate().position(offsets[1]), offsets[0]);
-                            }
-
-                            @Override
-                            public _edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>> core(_edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>... e) {
-                                return new _edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>() {
-                                    @Override
-                                    public Set<traits> core(_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>... e) {
-                                        return flaggs[0];
-                                    }
-
-                                    @Override
-                                    protected _edge<UnaryOperator<ByteBuffer>, Integer> at() {
-                                        return r$();
-                                    }
-
-                                    @Override
-                                    protected _edge<UnaryOperator<ByteBuffer>, Integer> goTo(_edge<UnaryOperator<ByteBuffer>, Integer> unaryOperatorInteger_edge) {
-                                        throw new Error("cant move this");
-                                    }
-
-                                    @Override
-                                    protected _edge<UnaryOperator<ByteBuffer>, Integer> r$() {
-                                        return new _edge<UnaryOperator<ByteBuffer>, Integer>() {
-                                            @Override
-                                            protected Integer at() {
-                                                return r$();
-                                            }
-
-                                            @Override
-                                            protected Integer goTo(Integer integer) {
-                                                throw new Error("immutable");
-                                            }
-
-                                            @Override
-                                            public UnaryOperator<ByteBuffer> core(_edge<UnaryOperator<ByteBuffer>, Integer>... e) {
-                                                return op;
-                                            }
-
-                                            @Override
-                                            protected Integer r$() {
-                                                return offsets[1];
-                                            }
-                                        };
-                                    }
-                                };
-                            }
-                        };
+                        return new _edge_ptr_edge(buffer, offsets, flaggs, op);
 
                     }
                 }).filter(
@@ -174,5 +104,88 @@ public class anyOf_ {
         };
     }
 
+    private static class _edge_ptr_edge extends _edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr> {
+        private final ByteBuffer buffer;
+        private final int[] offsets;
+        private final Set[] flaggs;
+        private final UnaryOperator<ByteBuffer> op;
+
+        public _edge_ptr_edge(ByteBuffer buffer, int[] offsets, Set[] flaggs, UnaryOperator<ByteBuffer> op) {
+            this.buffer = buffer;
+            this.offsets = offsets;
+            this.flaggs = flaggs;
+            this.op = op;
+        }
+
+        @Override
+        protected _ptr at() {
+            return r$();
+        }
+
+        @Override
+        protected _ptr goTo(_ptr ptr) {
+            throw new Error("trifling with an immutable pointer");
+        }
+
+        /**
+         * this binds a pointer to a pair of ByteBuffer and Integer.  note the bytebuffer is mutated by this
+         * operation and will corrupt the source stream if this isn't a slice or a duplicate
+         *
+         *
+         * @return the _ptr
+         */
+        @Override
+
+        protected _ptr r$() {
+
+            return (_ptr) new _ptr().bind(
+                    (ByteBuffer) buffer.duplicate().position(offsets[1]), offsets[0]);
+        }
+
+        @Override
+        public _edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>> core(_edge<_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>, _ptr>... e) {
+            return new _edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>() {
+                @Override
+                public Set<traits> core(_edge<Set<traits>, _edge<UnaryOperator<ByteBuffer>, Integer>>... e) {
+                    return flaggs[0];
+                }
+
+                @Override
+                protected _edge<UnaryOperator<ByteBuffer>, Integer> at() {
+                    return r$();
+                }
+
+                @Override
+                protected _edge<UnaryOperator<ByteBuffer>, Integer> goTo(_edge<UnaryOperator<ByteBuffer>, Integer> unaryOperatorInteger_edge) {
+                    throw new Error("cant move this");
+                }
+
+                @Override
+                protected _edge<UnaryOperator<ByteBuffer>, Integer> r$() {
+                    return new _edge<UnaryOperator<ByteBuffer>, Integer>() {
+                        @Override
+                        protected Integer at() {
+                            return r$();
+                        }
+
+                        @Override
+                        protected Integer goTo(Integer integer) {
+                            throw new Error("immutable");
+                        }
+
+                        @Override
+                        public UnaryOperator<ByteBuffer> core(_edge<UnaryOperator<ByteBuffer>, Integer>... e) {
+                            return op;
+                        }
+
+                        @Override
+                        protected Integer r$() {
+                            return offsets[1];
+                        }
+                    };
+                }
+            };
+        }
+    }
 }
 
